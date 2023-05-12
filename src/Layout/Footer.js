8bd36b23a1ad41efdd8aa667f81504e76dc3f8c9
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-12 19:47:21 +0500

    add footer__img

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -3,10 +3,20 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <footer className="page-footer">
-        <div className="footer-copyright">
-          <div className="container">
-            <h3>&copy; {new Date().getFullYear()} Copyright Reserved</h3>
+      <footer className="footer">
+        <div className="footer__img">
+          <a href="#!">
+            <img
+              src="https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/play-icon-18-256.png"
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="page-footer">
+          <div className="footer-copyright">
+            <div className="container">
+              <h3>&copy; {new Date().getFullYear()} Copyright Reserved</h3>
+            </div>
           </div>
         </div>
       </footer>
