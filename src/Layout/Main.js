c75227af60b43df4aca10e88ee402f0b18e13bef
uabdirasulm <ubbiniyazov04@gmail.com>
2023-05-12 15:11:44 +0500

    add fetch

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 
 class Main extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?i=tt3896198&apikey=8534e0c0&s=black")
+      .then((data) => data.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+
   render() {
-    return (
-      <main>
-        <h1>Main content</h1>
-      </main>
-    );
+    return <main></main>;
   }
 }
 
